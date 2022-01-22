08b2ab49482bedc25b09954263ebba57cf3cf148
Juan Francisco Delledonne <Delfrank94@gmail.com>
2022-01-22 01:28:29 -0300

    add project

---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var velocity : Vector2 = Vector2.ZERO
+export var runSpeed = 100 
+export var jumpSpeed = 200
+export var gravity = 500
+var motion = 0
+
+func _physics_process(delta):
+	motion = Input.get_action_strength("mRigth") -Input.get_action_strength("mLeft")
+	velocity.x = lerp(velocity.x, motion* runSpeed, 0.3)
+	if !is_on_floor():
+		velocity.y += gravity
+	if Input.is_action_just_pressed("mUp") && is_on_floor():
+		velocity.y = -jumpSpeed
+	velocity = move_and_slide(velocity,Vector2.UP)
+	print(velocity)
